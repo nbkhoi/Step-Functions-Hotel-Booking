6859ea2237cd0856d0b47a525056fa70d9b6938a
Khoi Nguyen Bao <nbkhoi@tma.com.vn>
2024-07-08 09:15:10 +0700

    Initial commit

---
+++ tests/unit/test_reservation_confirmation_wf_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from reservation_confirmation_wf.reservation_confirmation_wf_stack import ReservationConfirmationWfStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in reservation_confirmation_wf/reservation_confirmation_wf_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = ReservationConfirmationWfStack(app, "reservation-confirmation-wf")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
